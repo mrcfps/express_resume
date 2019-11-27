2b84923d4b618a3c76aee1d64de2011ea7c4dd41
mRcfps <1402491442@qq.com>
2019-11-27 21:27:22 +0800

    用 Express 搭建服务器

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const http = require('http');
+const express = require('express');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('Hello World\n');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
