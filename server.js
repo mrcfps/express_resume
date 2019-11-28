f331722c1021c335a42d2129fa3f76b815c9d2e4
mRcfps <1402491442@qq.com>
2019-11-28 15:55:08 +0800

    编写第一个中间件

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+
+function loggingMiddleware(req, res, next) {
+  const time = new Date();
+  console.log(`[${time.toLocaleString()}] ${req.method} ${req.url}`);
+  next();
+}
+
+app.use(loggingMiddleware);
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
